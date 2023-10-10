58263baaf1675fc36510a64e5294e93f5739c0e3
Dylan Alexander <dylanjalexander2@gmail.com>
2023-10-10 13:51:17 -0400

    Initial Commit

---
+++ Disaster Response Optimization/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Disaster_Response_Optimization.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
