1d713c214b7b88bdce28c73cccd2e8e105ec699d
Dylan Alexander <dylanjalexander2@gmail.com>
2023-11-10 04:43:11 -0500

    Polygon is now drawn around center of queried zip code

--- Disaster Response Optimization/wwwroot/js/mapModule.js
+++ Disaster Response Optimization/wwwroot/js/mapModule.js
@@ -1,35 +1,10 @@
 ﻿// Global variable to hold the selected responder location
 let selectedResponderLocation = null;
 
+window.queryDisaster = queryDisaster;
 window.panToZipCode = panToZipCode;
 window.showResponders = showResponders;
-window.queryDisaster = queryDisaster;
 window.calculateRoute = calculateRoute;
-
-export function panToZipCode(zipCode) {
-    // Use the geocoder to get the latitude and longitude for the zipCode
-    window.geocoder.geocode({ 'address': zipCode }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            // Assuming the first result is the one we want
-            let latLng = results[0].geometry.location;
-
-            // Panning to the given latLng
-            map.panTo(latLng);
-
-            // Zooming in for a closer look
-            map.setZoom(10);
-        } else {
-            alert("Geocode was not successful for the following reason: " + status);
-        }
-    });
-}
-
-export function showResponders() {
-    // Show first responders logic here
-
-    // Enable "Calculate Shortest Route" button after responders are shown
-    document.getElementById('calculateRoute').disabled = false;
-}
 
 export function queryDisaster() {
     const state = document.getElementById('stateSelector').value;
@@ -44,6 +19,7 @@ export function queryDisaster() {
             return response.text();
         })
         .then(zipCode => {
+            console.log("Disaster was successfully queried. Queried zip code: " + zipCode);
             panToZipCode(zipCode);
             document.getElementById('showResponders').disabled = false;
         })
@@ -53,13 +29,87 @@ export function queryDisaster() {
         });
 }
 
+// Constants for calculations
+const EARTH_RADIUS = 6371; // radius in kilometers
+const MI_TO_KM = 1.60934; // miles to kilometers conversion factor
+
+function calculateOffsetCoordinates(centerLatLng, distanceMi) {
+    const distanceKm = distanceMi * MI_TO_KM;
+    const latOffset = distanceKm / EARTH_RADIUS * (180 / Math.PI);
+    const lonOffset = distanceKm / (EARTH_RADIUS * Math.cos(Math.PI * centerLatLng.lat / 180)) * (180 / Math.PI);
+
+    return {
+        north: centerLatLng.lat + latOffset,
+        south: centerLatLng.lat - latOffset,
+        east: centerLatLng.lng + lonOffset,
+        west: centerLatLng.lng - lonOffset
+    };
+}
+
+
+function drawSquarePolygon(centerLatLng, map) {
+    // Calculate the corner points
+    const { north, south, east, west } = calculateOffsetCoordinates(centerLatLng, 5);
+
+    // Define the coordinates of the polygon
+    const squareCoords = [
+        { lat: north, lng: east },  // NE corner
+        { lat: north, lng: west },  // NW corner
+        { lat: south, lng: west },  // SW corner
+        { lat: south, lng: east }   // SE corner
+    ];
+
+    // Construct the polygon
+    const squarePolygon = new google.maps.Polygon({
+        paths: squareCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
+
+    squarePolygon.setMap(map);
+}
+
+
+export function panToZipCode(zipCode) {
+    // Use the geocoder to get the latitude and longitude for the zipCode
+    window.geocoder.geocode({ 'address': zipCode }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            // Assuming the first result is the one we want
+            let latLng = results[0].geometry.location;
+
+            // Panning to the given latLng
+            window.map_ui.panTo(latLng);
+
+            // Zooming in for a closer look
+            window.map_ui.setZoom(10);
+
+            // Draw the square with 5 mile radius from the center of the zip code.
+            drawSquarePolygon(latLng.toJSON(), window.map_ui);
+
+            console.log("Map was panned to queried zip code, and square polygon drawn.")
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+    });
+}
+
+export function showResponders() {
+    // Show first responders logic here
+
+    // Enable "Calculate Shortest Route" button after responders are shown
+    document.getElementById('calculateRoute').disabled = false;
+}
+
 export function calculateRoute() {
     // You can implement the logic for calculating and displaying the shortest route here.
     // You may use Google Maps Directions Service for this.
     // Placeholder logic:
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
-    directionsRenderer.setMap(map);
+    directionsRenderer.setMap(window.map_ui);
 
     const start = new google.maps.LatLng(37.7749, -122.4194); // Example start location
     const end = new google.maps.LatLng(37.7849, -122.4294); // Example end location
--- Disaster Response Optimization/wwwroot/js/site.js
+++ Disaster Response Optimization/wwwroot/js/site.js
@@ -1,16 +1,18 @@
 ﻿// Initialize the map first
-let map;
+let map_ui;
 // Initialize the Geocoder
 let geocoder;
 
 
 window.initMap = function() {
     const centerLocation = { lat: 39.8283, lng: -98.5795 }; // Geographic center of the contiguous U.S.
-    map = new google.maps.Map(document.getElementById('map'), {
+    window.map_ui = new google.maps.Map(document.getElementById('map'), {
         center: centerLocation,
         zoom: 4, // A zoom level of 4 should show most of the U.S.
     });
+    console.log("Map has been initialized.")
 
     // Initialize the geocoder
     window.geocoder = new google.maps.Geocoder();
+    console.log("(Google Maps API) Geocoder object initialized.")
 }
