31e5f8d3ad4d0e93da3e08c645165f874ff15641
Dylan Alexander <dylanjalexander2@gmail.com>
2023-11-10 04:00:16 -0500

    Now returning queried zip codes from dataset to clientside

---
+++ Infastructure/CsvConfig.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using System;
+using System.Globalization;
+
+namespace Disaster_Response_Optimization.Infrastructure.CsvConfig
+{
+    public class CustomDateTimeConverter : DateTimeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (string.IsNullOrWhiteSpace(text) || text.Equals("NA"))
+            {
+                return null; // Return null for a nullable DateTime
+            }
+
+            // Adjust the format if your date is in a specific format, like ISO 8601
+            var formats = new[] {
+                "yyyy-MM-ddTHH:mm:ssZ",
+                "yyyy-MM-ddTHH:mm:ss.fffZ",
+                CultureInfo.InvariantCulture.DateTimeFormat.SortableDateTimePattern
+            };
+
+            if (DateTime.TryParseExact(text, formats, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var date))
+            {
+                return date;
+            }
+
+            throw new TypeConverterException(this, memberMapData, text, row.Context, $"The value '{text}' cannot be parsed as DateTime.");
+        }
+    }
+}
--- Infastructure/Data.cs
+++ Infastructure/Data.cs
@@ -1,29 +1,34 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
+using Disaster_Response_Optimization.Infrastructure.CsvConfig;
 using System.Globalization;
 
 namespace Disaster_Response_Optimization.Infastructure.Data
 {
     public class DisasterDeclaration
     {
-        public string? FemaDeclarationString { get; set; }
-        public int DisasterNumber { get; set; }
-        public string? State { get; set; }
-        public string? DeclarationType { get; set; }
-        public DateTime DeclarationDate { get; set; }
-        public int FyDeclared { get; set; }
-        public string? IncidentType { get; set; }
-        public string? DeclarationTitle { get; set; }
-        public bool IhProgramDeclared { get; set; }
-        public bool IaProgramDeclared { get; set; }
-        public bool PaProgramDeclared { get; set; }
-        public bool HmProgramDeclared { get; set; }
-        public DateTime IncidentBeginDate { get; set; }
-        public DateTime? IncidentEndDate { get; set; }
-        public DateTime? DisasterCloseoutDate { get; set; }
-        public string? Fips { get; set; }
-        public int PlaceCode { get; set; }
-        public string? DesignatedArea { get; set; }
+        public string? fema_declaration_string { get; set; }
+        public int disaster_number { get; set; }
+        public string? state { get; set; }
+        public string? declaration_type { get; set; }
+        public DateTime declaration_date { get; set; }
+        public int fy_declared { get; set; }
+        public string? incident_type { get; set; }
+        public string? declaration_title { get; set; }
+        public bool ih_program_declared { get; set; }
+        public bool ia_program_declared { get; set; }
+        public bool pa_program_declared { get; set; }
+        public bool hm_program_declared { get; set; }
+        public DateTime incident_begin_date { get; set; }
+        public DateTime? incident_end_date { get; set; }
+        public DateTime? disaster_closeout_date { get; set; }
+        public string? fips { get; set; }
+        public int place_code { get; set; }
+        public string? designated_area { get; set; }
+        public DateTime? last_ia_filing_date { get; set; }
+        public DateTime? last_refresh { get; set; }
+        public string? hash { get; set; }
+        public string? id { get; set; }
     }
 
     // Interface defines the contract for this service.
@@ -46,24 +51,45 @@ namespace Disaster_Response_Optimization.Infastructure.Data
         private List<DisasterDeclaration> LoadDisasterDeclarations()
         {
             using (var reader = new StreamReader(_filePath))
-            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                // Configuration to match your CSV format
-                HasHeaderRecord = true,
-                Delimiter = ","
-            }))
-            {
-                return csv.GetRecords<DisasterDeclaration>().ToList();
+                var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = true,
+                    Delimiter = ",",
+                    MissingFieldFound = null // To ignore missing fields
+                };
+
+                using (var csv = new CsvReader(reader, csvConfig))
+                {
+                    csv.Context.TypeConverterCache.AddConverter<DateTime?>(new CustomDateTimeConverter());
+
+                    return csv.GetRecords<DisasterDeclaration>().ToList();
+                }
             }
         }
 
         public string? GetMajorDisasterZipCode(string state, string incidentType)
         {
+            // Assume that the presence of any declared program indicates the severity of the disaster.
+            // More programs declared could imply a more severe disaster.
             var declaration = _declarations
-                .Where(d => d.State == state && d.IncidentType == incidentType && d.DeclarationType == "DR")
+                .Where(d => d.state == state && d.incident_type == incidentType && d.declaration_type == "DR")
+                .OrderByDescending(d => d.ih_program_declared)
+                .ThenByDescending(d => d.ia_program_declared)
+                .ThenByDescending(d => d.pa_program_declared)
+                .ThenByDescending(d => d.hm_program_declared)
+                .ThenBy(d => d.declaration_date) // If you want the earliest disaster in case of a tie on severity.
                 .FirstOrDefault();
 
-            return declaration?.Fips; // Return 5-digit zip code
+            // Assuming the Fips field contains the ZIP code directly.
+            // If it contains a county code or similar, additional logic will be needed.
+            string? zipCode = null;
+            if (declaration != null && declaration.fips?.Length == 5)
+            {
+                zipCode = declaration.fips;
+            }
+
+            return zipCode;
         }
     }
 }
